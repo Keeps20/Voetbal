b0b0d0f35a466980de9b0dbe8096852b0ec23887
Gaby Hageman <gabyhageman2@gmail.com>
2019-12-01 21:11:38 +0100

    start

---
+++ application.py
@@ -0,0 +1,31 @@
+import os
+import json
+import requests
+
+from flask import Flask, session, render_template, request, redirect, abort
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+user = []
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+@app.route("/")
+def index():
+    """ Pagina bij het openen van de website """
+
+    return render_template("homepage.html") 
